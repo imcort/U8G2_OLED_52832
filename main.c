588d74df75ff51b4833e2e8de3032350db9e7e1a
imcort <179376503@qq.com>
2019-09-30 14:53:24 +0800

    first

---
+++ main.c
@@ -0,0 +1,231 @@
+/**
+ * Copyright (c) 2015 - 2018, Nordic Semiconductor ASA
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form, except as embedded into a Nordic
+ *    Semiconductor ASA integrated circuit in a product or a software update for
+ *    such product, must reproduce the above copyright notice, this list of
+ *    conditions and the following disclaimer in the documentation and/or other
+ *    materials provided with the distribution.
+ *
+ * 3. Neither the name of Nordic Semiconductor ASA nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * 4. This software, with or without modification, must only be used with a
+ *    Nordic Semiconductor ASA integrated circuit.
+ *
+ * 5. Any software provided in binary form under this license must not be reverse
+ *    engineered, decompiled, modified and/or disassembled.
+ *
+ * THIS SOFTWARE IS PROVIDED BY NORDIC SEMICONDUCTOR ASA "AS IS" AND ANY EXPRESS
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY, NONINFRINGEMENT, AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL NORDIC SEMICONDUCTOR ASA OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+/** @file
+ * @defgroup tw_sensor_example main.c
+ * @{
+ * @ingroup nrf_twi_example
+ * @brief TWI Sensor Example main file.
+ *
+ * This file contains the source code for a sample application using TWI.
+ *
+ */
+
+#include <stdio.h>
+#include "boards.h"
+#include "app_util_platform.h"
+#include "app_error.h"
+#include "nrf_drv_twi.h"
+#include "nrf_delay.h"
+
+#include "u8g2.h"
+#include "u8x8.h"
+
+#include "nrf_log.h"
+#include "nrf_log_ctrl.h"
+#include "nrf_log_default_backends.h"
+
+/* TWI instance ID. */
+#define TWI_INSTANCE_ID     0
+
+#define ARDUINO_SCL_PIN     8
+#define ARDUINO_SDA_PIN     6
+
+/* Indicates if operation on TWI has ended. */
+static volatile bool m_xfer_done = false;
+
+/* TWI instance. */
+static const nrf_drv_twi_t m_twi = NRF_DRV_TWI_INSTANCE(TWI_INSTANCE_ID);
+
+///**
+// * @brief TWI events handler.
+// */
+//void twi_handler(nrf_drv_twi_evt_t const * p_event, void * p_context)
+//{
+//    switch (p_event->type)
+//    {
+//        case NRF_DRV_TWI_EVT_DONE:
+//						NRF_LOG_INFO("TRUE");
+//            m_xfer_done = true;
+//            break;
+//        default:
+//            break;
+//    }
+//}
+
+/**
+ * @brief UART initialization.
+ */
+void twi_init (void)
+{
+    ret_code_t err_code;
+
+    const nrf_drv_twi_config_t twi_oled_config = {
+       .scl                = ARDUINO_SCL_PIN,
+       .sda                = ARDUINO_SDA_PIN,
+       .frequency          = NRF_DRV_TWI_FREQ_100K,
+       .interrupt_priority = APP_IRQ_PRIORITY_HIGH,
+       .clear_bus_init     = false
+    };
+
+    err_code = nrf_drv_twi_init(&m_twi, &twi_oled_config, NULL, NULL);
+    APP_ERROR_CHECK(err_code);
+
+    nrf_drv_twi_enable(&m_twi);
+}
+
+uint8_t u8x8_gpio_and_delay_template(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, void *arg_ptr)
+{
+  switch(msg)
+  {
+    case U8X8_MSG_GPIO_AND_DELAY_INIT:	// called once during init phase of u8g2/u8x8
+			nrf_delay_ms(1);
+      break;							// can be used to setup pins
+    case U8X8_MSG_DELAY_MILLI:			// delay arg_int * 1 milli second
+			nrf_delay_ms(arg_int);
+      break;
+    case U8X8_MSG_DELAY_I2C:				// arg_int is the I2C speed in 100KHz, e.g. 4 = 400 KHz
+			nrf_delay_us(arg_int);
+      break;							// arg_int=1: delay by 5us, arg_int = 4: delay by 1.25us
+    default:
+      u8x8_SetGPIOResult(u8x8, 1);			// default return value
+      break;
+  }
+  return 1;
+}
+
+uint8_t u8x8_byte_i2c(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, void *arg_ptr)
+{
+  static uint8_t buffer[32];		/* u8g2/u8x8 will never send more than 32 bytes between START_TRANSFER and END_TRANSFER */
+  static uint8_t buf_idx;
+  uint8_t *data;
+	ret_code_t err_code;
+	
+  switch(msg)
+  {
+    case U8X8_MSG_BYTE_SEND:
+      data = (uint8_t *)arg_ptr;      
+      while( arg_int > 0 )
+      {
+				buffer[buf_idx++] = *data;
+				data++;
+				arg_int--;
+      }      
+      break;
+    case U8X8_MSG_BYTE_INIT:
+			twi_init();
+			NRF_LOG_INFO("INIT IIC.");
+      break;
+    case U8X8_MSG_BYTE_START_TRANSFER:
+      buf_idx = 0;
+      break;
+    case U8X8_MSG_BYTE_END_TRANSFER:
+      err_code = nrf_drv_twi_tx(&m_twi, u8x8_GetI2CAddress(u8x8) >> 1, buffer, buf_idx, false);
+			APP_ERROR_CHECK(err_code);
+//			do
+//			{
+//					__WFE();
+//			}while (m_xfer_done == false);
+      break;
+    default:
+      return 0;
+  }
+  return 1;
+}
+
+void draw(u8g2_t *u8g2)
+{
+    u8g2_SetFontMode(u8g2, 1);  // Transparent
+    u8g2_SetFontDirection(u8g2, 0);
+    u8g2_SetFont(u8g2, u8g2_font_inb24_mf);
+    u8g2_DrawStr(u8g2, 0, 20, "U");
+    
+    u8g2_SetFontDirection(u8g2, 1);
+    u8g2_SetFont(u8g2, u8g2_font_inb30_mn);
+    u8g2_DrawStr(u8g2, 21,8,"8");
+        
+    u8g2_SetFontDirection(u8g2, 0);
+    u8g2_SetFont(u8g2, u8g2_font_inb24_mf);
+    u8g2_DrawStr(u8g2, 51,30,"g");
+    u8g2_DrawStr(u8g2, 67,30,"\xb2");
+    
+    u8g2_DrawHLine(u8g2, 2, 35, 47);
+    u8g2_DrawHLine(u8g2, 3, 36, 47);
+    u8g2_DrawVLine(u8g2, 45, 32, 12);
+    u8g2_DrawVLine(u8g2, 46, 33, 12);
+  
+    u8g2_SetFont(u8g2, u8g2_font_4x6_tr);
+    u8g2_DrawStr(u8g2, 1,54,"github.com/olikraus/u8g2");
+}
+
+/**
+ * @brief Function for main application entry.
+ */
+int main(void)
+{
+		NRF_POWER->DCDCEN = 1;
+	
+    APP_ERROR_CHECK(NRF_LOG_INIT(NULL));
+    NRF_LOG_DEFAULT_BACKENDS_INIT();
+
+    NRF_LOG_INFO("\r\nTWI sensor example started.");
+    NRF_LOG_FLUSH();
+    
+		static u8g2_t u8g2;
+		u8g2_Setup_ssd1306_i2c_128x64_noname_1(&u8g2, U8G2_R0, u8x8_byte_i2c, u8x8_gpio_and_delay_template); 
+		u8g2_InitDisplay(&u8g2);
+		u8g2_SetPowerSave(&u8g2, 0);
+	
+		u8g2_FirstPage(&u8g2);
+		do
+		{
+				draw(&u8g2);
+		} while (u8g2_NextPage(&u8g2));
+		
+    while (true)
+    {
+         /* USER CODE END WHILE */
+				
+				/* USER CODE BEGIN 3 */
+				__WFE();
+        NRF_LOG_FLUSH();
+    }
+}
+
+/** @} */
